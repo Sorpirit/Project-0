7bb1a929f9dc9f6e681c39edca224c892ac8fa00
Sorpirit <danvuk11@gmail.com>
2019-09-16 22:46:08 +0300

    keyboard movement control

---
+++ Assets/Scripts/MovmentController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovmentController : MonoBehaviour
+{
+    public string HorizAxis;
+    public string VertAxis;
+
+    public float turnSpeed;
+    public float moveSpeed;
+
+    public Rigidbody2D body;
+
+    public bool invert;
+
+    private float turnVal;
+    private float accVal;
+
+    private void Update()
+    {
+        turnVal = -Input.GetAxis(HorizAxis);
+        turnVal *= invert ? -1 : 1;
+        accVal = Input.GetAxis(VertAxis);
+    }
+
+    private void FixedUpdate()
+    { 
+        Rotate();
+        Move();
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(transform.forward,turnSpeed * turnVal);
+    }
+
+    private void Move()
+    {
+        body.velocity += (Vector2) transform.up * accVal * moveSpeed * Time.deltaTime;
+    }
+}
