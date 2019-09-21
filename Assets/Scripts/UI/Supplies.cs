d5db3d84909760be6822812285c4f3cf3961467d
nikinov <novellenicholas@gmail.com>
2019-09-21 20:02:15 +0200

    Adding Supplies System
    so there are Storage modules in the game one for each type of supplie (Oxygen, Water, Food, Energy) you can set the max capacity for each module in the Supplies Manager you can duplicate any Modules and also the number that you set in the supplie manager will be only for one module so the total number will be the number* amount of modules for that supply yoou can go to the supplie drop down menu in the game and click on the individual supplies to add 1 later on we will add the number when the player mines an astroids or gets the supplie some other way

---
+++ Assets/Scripts/UI/Supplies.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Supplies : MonoBehaviour
+{
+    int Oxygen;
+    int Water;
+    int Food;
+    int Energy;
+    int OxygenStorage;
+    int WaterStorage;
+    int FoodStorage;
+    int EnergyStorage;
+    public int OxygenStorageModule = 50;
+    public int WaterStorageModule = 50;
+    public int FoodStorageModule = 50;
+    public int EnergyStorageModule = 50;
+    int OxygenStorageModuleAmount;
+    int WaterStorageModuleAmount;
+    int FoodStorageModuleAmount;
+    int EnergyStorageModuleAmount;
+    GameObject[] OxStorageModule;
+    GameObject[] WaStorageModule;
+    GameObject[] FoStorageModule;
+    GameObject[] EnStorageModule;
+    public Text OxygenText;
+    public Text WaterText;
+    public Text FoodText;
+    public Text EnergyText;
+
+    void Update()
+    {
+        //Finding Oxygen Storage Modules and Making sure that there isnt more Oxygen then the storage you have for it
+        OxStorageModule = GameObject.FindGameObjectsWithTag("OxygenStorageModule");
+        OxygenStorageModuleAmount = 0;
+        foreach (GameObject O in OxStorageModule)
+        {
+            OxygenStorageModuleAmount += 1;
+        }
+        OxygenStorage = OxygenStorageModule * OxygenStorageModuleAmount;
+        if (Oxygen >= OxygenStorage)
+        {
+            Oxygen = OxygenStorage;
+        }
+        OxygenText.text = "Oxygen: " + Oxygen;
+        //Finding Water Storage Modules and Making sure that there isnt more Water then the storage you have for it
+        WaStorageModule = GameObject.FindGameObjectsWithTag("WaterStorageModule");
+        WaterStorageModuleAmount = 0;
+        foreach (GameObject O in WaStorageModule)
+        {
+            WaterStorageModuleAmount += 1;
+        }
+        WaterStorage = WaterStorageModule * WaterStorageModuleAmount;
+        if (Water >= WaterStorage)
+        {
+            Water = WaterStorage;
+        }
+        WaterText.text = "Water: " + Water;
+        //Finding Food Storage Modules and Making sure that there isnt more Water then the Food you have for it
+        FoStorageModule = GameObject.FindGameObjectsWithTag("FoodStorageModule");
+        FoodStorageModuleAmount = 0;
+        foreach (GameObject O in FoStorageModule)
+        {
+            FoodStorageModuleAmount += 1;
+        }
+        FoodStorage = FoodStorageModule * FoodStorageModuleAmount;
+        if (Food >= FoodStorage)
+        {
+            Food = FoodStorage;
+        }
+        FoodText.text = "Food: " + Food;
+        //Finding Energy Storage Modules and Making sure that there isnt more Energy then the storage you have for it
+        EnStorageModule = GameObject.FindGameObjectsWithTag("EnergyStorageModule");
+        EnergyStorageModuleAmount = 0;
+        foreach (GameObject O in EnStorageModule)
+        {
+            EnergyStorageModuleAmount += 1;
+        }
+        EnergyStorage = EnergyStorageModule * EnergyStorageModuleAmount;
+        if (Energy >= EnergyStorage)
+        {
+            Energy = EnergyStorage;
+        }
+        EnergyText.text = "Energy: " + Energy;
+    }
+    public void AddOxygen()
+    {
+        Oxygen += 1;
+    }
+    public void AddWater()
+    {
+        Water += 1;
+    }
+    public void AddFood()
+    {
+        Food += 1;
+    }
+    public void AddEnergy()
+    {
+        Energy += 1;
+    }
+}
