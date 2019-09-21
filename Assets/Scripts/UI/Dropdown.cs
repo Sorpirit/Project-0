e9b7f6a9f7252de241dcabea77750e6eade62307
nikinov <novellenicholas@gmail.com>
2019-09-21 10:17:41 +0200

    Adding working Dropdown Menu

---
+++ Assets/Scripts/UI/Dropdown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dropdown : MonoBehaviour, IPointerClickHandler
+{
+    public RectTransform container;
+    public bool isOpen;
+    public int i;
+    void Start()
+    {
+        container = transform.FindChild("Container").GetComponent<RectTransform>();
+        isOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 scale = container.localScale;
+        scale.y = Mathf.Lerp(scale.y, isOpen ? 1 : 0, Time.deltaTime * i);
+        container.localScale = scale;
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (isOpen == true)
+        {
+            isOpen = false;
+        }
+        else
+        {
+            isOpen = true;
+        }
+    }
+}
