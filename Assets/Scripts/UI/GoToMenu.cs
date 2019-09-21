f39dce8e7a10e3d597208fdecd25761b8fa2968d
nikinov <novellenicholas@gmail.com>
2019-09-21 14:23:08 +0200

    Adding Settings to Sample Scene
    Pause button Pause menu Settings in pause menu

---
+++ Assets/Scripts/UI/GoToMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMenu : MonoBehaviour
+{
+    public void GoToMainMenu ()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 0);
+        Debug.Log("going to main menu...");
+    }
+}
---
+++ Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PausePanelUI;
+    public GameObject PauseButtonUI;
+    public GameObject ResumeButtonUI;
+    public GameObject JustSettingsUI;
+    public GameObject ResumeButtonToUI;
+    private void Start()
+    {
+        PausePanelUI.SetActive(false);
+        ResumeButtonUI.SetActive(false);
+        JustSettingsUI.SetActive(false);
+        ResumeButtonToUI.SetActive(false);
+        PauseButtonUI.SetActive(true);
+}
+    public void OpenPause()
+    {
+        Debug.Log("pause");
+        PausePanelUI.SetActive(true);
+        PauseButtonUI.SetActive(false);
+        ResumeButtonUI.SetActive(true);
+    }
+    public void ClosePause()
+    {
+        Debug.Log("Resume");
+        PausePanelUI.SetActive(false);
+        PauseButtonUI.SetActive(true);
+        ResumeButtonUI.SetActive(false);
+        JustSettingsUI.SetActive(false);
+
+    }
+    public void OpenSettings()
+    {
+        Debug.Log("going to settings");
+        PausePanelUI.SetActive(false);
+        PauseButtonUI.SetActive(false);
+        ResumeButtonUI.SetActive(false);
+        JustSettingsUI.SetActive(true);
+        ResumeButtonToUI.SetActive(true);
+    }
+}
